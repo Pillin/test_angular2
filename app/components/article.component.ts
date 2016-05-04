c80f6c1840be493c58dc5fc0c0e47ea492b54fc2
Ignacia <ignacia.perez@yalea.com>
2016-05-04 18:38:16 -0300

    pass variable to template article

--- app/components/article.component.ts
+++ app/components/article.component.ts
@@ -8,12 +8,14 @@ import { Article } from '../models/article';
     <md-card>
 	   <md-card-header>
 	      <img md-card-avatar src="https://pixabay.com/static/uploads/photo/2014/12/17/21/30/wild-flowers-571940_960_720.jpg">
-	      <md-card-title>Header title</md-card-title>
-	      <md-card-subtitle>Header subtitle</md-card-subtitle>
+	      <md-card-title></md-card-title>
+	      <md-card-subtitle>{{art.title}}</md-card-subtitle>
 	   </md-card-header>
 	   <img md-card-image src="https://pixabay.com/static/uploads/photo/2014/12/17/21/30/wild-flowers-571940_960_720.jpg">
 	   <md-card-content>
-	      <p>Here is some more content</p>
+	   	  <p>{{art.introduction}}</p>
+	      <p>{{art.content}}</p>
+	      <s>{{art.owner}}</s>
 	   </md-card-content>
 	</md-card>`
 })
