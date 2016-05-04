7715b8d24d0c0041af5b26d3c78b2f84e59505da
Ignacia <ignacia.perez@yalea.com>
2016-05-04 18:39:37 -0300

    delete json providers

--- app/components/book.component.ts
+++ app/components/book.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HTTP_PROVIDERS, JSONP_PROVIDERS }    from '@angular/http';
+import { HTTP_PROVIDERS } from '@angular/http';
 import { Article } from '../models/article';
 import { ArticleService } from '../services/article.service'
 import { ArticleComponent } from './article.component';
@@ -11,7 +11,7 @@ import { ArticleComponent } from './article.component';
     <h1>{{title}}</h1>
     <article [art]="art" *ngFor="let art of articles" ></article>`,
     directives: [ArticleComponent],
-    providers: [HTTP_PROVIDERS, JSONP_PROVIDERS, ArticleService]
+    providers: [HTTP_PROVIDERS, ArticleService]
 })
 
 export class BookComponent implements OnInit {
@@ -22,10 +22,9 @@ export class BookComponent implements OnInit {
     constructor(private articleService: ArticleService) { }
 
     getArticles() {
-        /*this.articleService.getAllArticles().subscribe(
+        this.articleService.getAllArticles().subscribe(
             (articles:any) => this.articles = articles
-        );*/
-        this.articleService.getAllArticles().then(articles => this.articles = articles);
+        );
     }
 
     ngOnInit() {
